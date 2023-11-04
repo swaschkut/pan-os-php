783b450ad15c2875fd262889591c4817e0ed6e9a
Sven Waschkut <swaschkut@users.noreply.github.com>
2023-11-04 16:13:28 +0100

    db_conn.php | only if docker compose is used

--- utils/develop/ui/test/db_conn.php
+++ utils/develop/ui/test/db_conn.php
@@ -25,6 +25,7 @@ $pass = 'MYSQL_PASSWORD';
 
 // check the MySQL connection status
 
+/*
 $conn = new mysqli($host, $user, $pass, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -32,3 +33,4 @@ if ($conn->connect_error) {
     #echo "Connected to MySQL server successfully!";
 }
 
+*/
