ffe23cea3febcf890a174d526f0dce8c74c63b87
Sven Waschkut <swaschkut@users.noreply.github.com>
2024-07-03 07:25:29 +0200

    Create sophos_config_export.sh

---
+++ migration/parser/SOPHOS/sophos_config_export.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+###############################################################
+#request SOPHOS UTM configuration data
+#
+#
+###############################################################
+
+apikey="APIKEY from SOPHOS UTM"
+fqdn="SOPHOS IP / FQDN"
+
+urlbase="https://$fqdn:4444/api/objects/"
+
+# Create a temporary directory for downloaded files
+TEMP_DIR=$(mktemp -d)
+OUTPUT FILE="merged output.txt"
+
+j=0
+err=0
+
+arrapi=("network/host/" \
+		"network/dns_host/" \
+		"network/dns_group/" \
+		"network/group/" \
+		"network/range/" \
+		"network/network/" \
+		"network/interface_network/" \
+		"service/group/" \
+		"service/tcp/" \
+		"service/udp/" \
+		"service/tcpudp/" \
+		"packetfilter/packetfilter/" \
+)
+
+
+for i in "${arrapi[@]}"
+do
+	curl -X GET -k -H "Accept: application/json" \
+				-H "Authorization: Basic $apikey" "$urlbase$i" -o "$TEMP_DIR"/file$j.txt
+	response_code=$?
+
+	if [ $response_code -eq 0 ]; then
+		echo "Downloaded $urlbase$i"
+	else
+		echo "Fehler beim Download von $urlbase$i"
+		let "err++"
+	fi
+	let "j++"
+done
+
+
+# Merge downloaded files into a single output file
+cat "$TEMP_DIR"/*.txt > "$OUTPUT_FILE"
+
+# Clean up temporary directory
+rm -rf "$TEMP DIR"
+
+################################################################
+# Rule Order
+##############################################################
+urlorder="https://$fqdn:4444/api/nodes/packetfilter.rules"
+
+curl -X GET -k -H "Accept: application/json" \
+				 -H "Authorization: Basic $apikey" "$urlorder" -o "ruleorder.txt"
+
+
