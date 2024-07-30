15eebbdb668b3094ac0e161db42f7535ece2a307
Sven Waschkut <swaschkut@users.noreply.github.com>
2024-07-30 10:31:01 +0200

    Update SOPHOSservice.php

--- migration/parser/SOPHOS/SOPHOSservice.php
+++ migration/parser/SOPHOS/SOPHOSservice.php
@@ -108,10 +108,11 @@ trait SOPHOSservice
                 {
                     $tmp_service_group = $this->sub->serviceStore->newServiceGroup($srv_name);
                 }
+                PH::print_stdout("Group found: '".$tmp_service_group->name()."'");
 
                 $tmp_found = FALSE;
                 $src_array = explode(",", $policy['members']);
-                print "- set group members: ";
+                print "\n- set group members: ";
                 foreach( $src_array as $members )
                 {
                     #print "src_ref:".$source."\n";
