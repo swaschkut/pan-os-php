927f1b4b5120d383e3aca6eb3520d98b98c5a63d
Sven Waschkut <swaschkut@users.noreply.github.com>
2024-08-28 17:41:15 +0200

    type=interface | introduce 'filter=(ipv4 includes 1.1.1.1/28)'

--- lib/misc-classes/filters/filters-Interface.php
+++ lib/misc-classes/filters/filters-Interface.php
@@ -14,6 +14,24 @@ RQuery::$defaultFilters['interface']['name']['operators']['eq'] = Array(
     )
 );
 
+RQuery::$defaultFilters['interface']['ipv4']['operators']['includes'] = Array(
+    'Function' => function(InterfaceRQueryContext $context )
+    {
+        $ip4_map = new IP4Map();
+        $ip4_addresses = $context->object->getLayer3IPv4Addresses();
+        foreach( $ip4_addresses as $ip4_address )
+        {
+            $ip4_map->addMap(IP4Map::mapFromText($ip4_address));
+        }
+
+        return $ip4_map->includesOtherMap( IP4Map::mapFromText($context->value) );
+    },
+    'arg' => true,
+    'ci' => Array(
+        'fString' => '(%PROP% ethernet1/1)',
+        'input' => 'input/panorama-8.0.xml'
+    )
+);
 
 
 // </editor-fold>
