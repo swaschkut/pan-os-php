51f25aa6dd3b0a11fc010e6c05b499c16ada42b8
Sven Waschkut <swaschkut@users.noreply.github.com>
2024-11-25 09:54:15 +0100

    update util action/filter | bp json file

--- utils/develop/bp/bp_create_json.php
+++ utils/develop/bp/bp_create_json.php
@@ -2,31 +2,13 @@
 
 $checkArray = array();
 
+$checkArray['virus'] = array();
 $checkArray['spyware'] = array();
 $checkArray['vulnerability'] = array();
-$checkArray['virus'] = array();
 
 
-$checkArray['spyware']['rule'] = array();
-$checkArray['spyware']['rule']['bp'] = array();
-$checkArray['spyware']['rule']['visibility'] = array();
-$checkArray['spyware']['rule']['bp']['severity'] = array('any', 'critical', 'high', 'medium');
-$checkArray['spyware']['rule']['bp']['action'] = array('reset-both');
-$checkArray['spyware']['rule']['bp']['packet-capture'] = array('single-packet', 'extended-capture');
-$checkArray['spyware']['rule']['visibility']['severity'] = array('any', 'critical', 'high', 'medium','low','informational');
-$checkArray['spyware']['rule']['visibility']['action'] = array('!allow');
-
-
-$checkArray['vulnerability']['rule'] = array();
-$checkArray['vulnerability']['rule']['bp'] = array();
-$checkArray['vulnerability']['rule']['visibility'] = array();
-$checkArray['vulnerability']['rule']['bp']['severity'] = array('any', 'critical', 'high', 'medium');
-$checkArray['vulnerability']['rule']['bp']['action'] = array('reset-both');
-$checkArray['vulnerability']['rule']['bp']['packet-capture'] = array('single-packet', 'extended-capture');
-$checkArray['vulnerability']['rule']['bp']['category-exclude'] = array('brute-force', 'app-id-change');
-$checkArray['vulnerability']['rule']['visibility']['severity'] = array('any', 'critical', 'high', 'medium','low','informational');
-$checkArray['vulnerability']['rule']['visibility']['action'] = array('!allow');
-
+######################################################################################################################
+######## RULE
 
 $checkArray['virus']['rule'] = array();
 $checkArray['virus']['rule']['bp'] = array();
@@ -52,6 +34,48 @@ $checkArray['virus']['rule']['bp']['mlav-action']['action'] = array('reset-both'
 $checkArray['virus']['rule']['bp']['mlav-action']['action-not-matching-type'] = array('reset-both');
 $checkArray['virus']['rule']['visibility']['mlav-action'] = array('!allow');
 
+####################
+
+$checkArray['spyware']['rule'] = array();
+$checkArray['spyware']['rule']['bp'] = array();
+$checkArray['spyware']['rule']['visibility'] = array();
+$checkArray['spyware']['rule']['bp']['severity'] = array('any', 'critical', 'high', 'medium');
+$checkArray['spyware']['rule']['bp']['action'] = array('reset-both');
+$checkArray['spyware']['rule']['bp']['packet-capture'] = array('single-packet', 'extended-capture');
+$checkArray['spyware']['rule']['visibility']['severity'] = array('any', 'critical', 'high', 'medium','low','informational');
+$checkArray['spyware']['rule']['visibility']['action'] = array('!allow');
+
+####################
+
+$checkArray['vulnerability']['rule'] = array();
+$checkArray['vulnerability']['rule']['bp'] = array();
+$checkArray['vulnerability']['rule']['visibility'] = array();
+$checkArray['vulnerability']['rule']['bp']['severity'] = array('any', 'critical', 'high', 'medium');
+$checkArray['vulnerability']['rule']['bp']['action'] = array('reset-both');
+$checkArray['vulnerability']['rule']['bp']['packet-capture'] = array('single-packet', 'extended-capture');
+$checkArray['vulnerability']['rule']['bp']['category-exclude'] = array('brute-force', 'app-id-change');
+$checkArray['vulnerability']['rule']['visibility']['severity'] = array('any', 'critical', 'high', 'medium','low','informational');
+$checkArray['vulnerability']['rule']['visibility']['action'] = array('!allow');
+
+
+######################################################################################################################
+######## SPYWARE DNS
+
+
+$checkArray['spyware']['dns'] = array();
+$checkArray['spyware']['dns']['bp'] = array();
+$checkArray['spyware']['dns']['bp']['action'] = array();
+$checkArray['spyware']['dns']['bp']['action'][0]['type'] = array('pan-dns-sec-malware','pan-dns-sec-phishing');
+$checkArray['spyware']['dns']['bp']['action'][0]['action'] = array('sinkhole');
+$checkArray['spyware']['dns']['bp']['action'][0]['packet-capture'] = array('single-packet');
+
+$checkArray['spyware']['dns']['bp']['action'][1]['type'] = array('pan-dns-sec-cc');
+$checkArray['spyware']['dns']['bp']['action'][1]['action'] = array('sinkhole');
+$checkArray['spyware']['dns']['bp']['action'][1]['packet-capture'] = array('extended-capture');
+
+
+######################################################################################################################
+######## CLOUD INLINE
 
 $checkArray['virus']['cloud-inline'] = array();
 $checkArray['spyware']['cloud-inline'] = array();
@@ -71,18 +95,11 @@ $checkArray['spyware']['cloud-inline']['visibility']['inline-policy-action'] = a
 $checkArray['vulnerability']['cloud-inline']['visibility']['inline-policy-action'] = array('!allow');
 
 
-$checkArray['spyware']['dns'] = array();
-$checkArray['spyware']['dns']['bp'] = array();
-$checkArray['spyware']['dns']['bp']['action'] = array();
-$checkArray['spyware']['dns']['bp']['action'][0]['type'] = array('pan-dns-sec-malware','pan-dns-sec-phishing');
-$checkArray['spyware']['dns']['bp']['action'][0]['action'] = array('sinkhole');
-$checkArray['spyware']['dns']['bp']['action'][0]['packet-capture'] = array('single-packet');
-
-$checkArray['spyware']['dns']['bp']['action'][1]['type'] = array('pan-dns-sec-cc');
-$checkArray['spyware']['dns']['bp']['action'][1]['action'] = array('sinkhole');
-$checkArray['spyware']['dns']['bp']['action'][1]['packet-capture'] = array('extended-capture');
-
+######################################################################################################################
 
 
 $json = json_encode($checkArray, JSON_PRETTY_PRINT);
+
+$fileName = "bp_sp_panw.json";
+file_put_contents($fileName, $json);
 echo $json;
