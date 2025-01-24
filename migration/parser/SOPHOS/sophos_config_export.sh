cc1340eea896df61287bda978133308a316a9e59
Sven Waschkut <swaschkut@users.noreply.github.com>
2025-01-24 15:34:50 +0100

    Update sophos_config_export.sh

--- migration/parser/SOPHOS/sophos_config_export.sh
+++ migration/parser/SOPHOS/sophos_config_export.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ###############################################################
 #request SOPHOS UTM configuration data
-#
-#
 ###############################################################
+
+echo "Request SOPHOS UTM configuration data"
 
 apikey="APIKEY from SOPHOS UTM"
 fqdn="SOPHOS IP / FQDN"
@@ -11,8 +11,11 @@ fqdn="SOPHOS IP / FQDN"
 urlbase="https://$fqdn:4444/api/objects/"
 
 # Create a temporary directory for downloaded files
-TEMP_DIR=$(mktemp -d)
-OUTPUT FILE="merged output.txt"
+
+# the temp directory used
+TEMP_DIR=$(mktemp -d -p ./)
+
+OUTPUT_FILE="merged output.txt"
 
 j=0
 err=0
@@ -35,7 +38,7 @@ arrapi=("network/host/" \
 for i in "${arrapi[@]}"
 do
 	curl -X GET -k -H "Accept: application/json" \
-				-H "Authorization: Basic $apikey" "$urlbase$i" -o "$TEMP_DIR"/file$j.txt
+				-H "Authorization: Basic $apikey" "$urlbase$i" -o "$TEMP_DIR/file$j.txt"
 	response_code=$?
 
 	if [ $response_code -eq 0 ]; then
@@ -49,10 +52,10 @@ done
 
 
 # Merge downloaded files into a single output file
-cat "$TEMP_DIR"/*.txt > "$OUTPUT_FILE"
+cat "$TEMP_DIR"/*.txt > "$fqdn-$OUTPUT_FILE"
 
 # Clean up temporary directory
-rm -rf "$TEMP DIR"
+rm -rf "$TEMP_DIR"
 
 ################################################################
 # Rule Order
@@ -60,6 +63,6 @@ rm -rf "$TEMP DIR"
 urlorder="https://$fqdn:4444/api/nodes/packetfilter.rules"
 
 curl -X GET -k -H "Accept: application/json" \
-				 -H "Authorization: Basic $apikey" "$urlorder" -o "ruleorder.txt"
+				 -H "Authorization: Basic $apikey" "$urlorder" -o "$fqdn-ruleorder.txt"
 
 
