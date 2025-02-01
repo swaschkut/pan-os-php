5903360b813fef9ad749dc62b068770822c0db60
Sven Waschkut <swaschkut@users.noreply.github.com>
2025-02-01 11:34:54 +0100

    SOPHOS UTM migration - extend config export script with relevant interface/routing information - add Sophos API pdf document

--- migration/parser/SOPHOS/sophos_config_export.sh
+++ migration/parser/SOPHOS/sophos_config_export.sh
@@ -2,11 +2,26 @@
 ###############################################################
 #request SOPHOS UTM configuration data
 ###############################################################
+#https://fqdn:4444/api/definitions
+
+
 
 echo "Request SOPHOS UTM configuration data"
 
+
 apikey="APIKEY from SOPHOS UTM"
+#NOT WORKING with USERNAME/PASSWORD
+#username="USERNAME from SOPHOS UTM"
+#password="PASSWORD from SOPHOS UTM"
 fqdn="SOPHOS IP / FQDN"
+
+apikey="YWRtaW46QWRtaW5AMTIzLg=="
+fqdn="192.168.10.185"
+#username="admin"
+#password="Admin@123."
+#authkey=$(echo -n $username + ":" + $password | base64)
+
+echo $authkey
 
 urlbase="https://$fqdn:4444/api/objects/"
 
@@ -20,25 +35,67 @@ OUTPUT_FILE="merged output.txt"
 j=0
 err=0
 
-arrapi=("network/host/" \
+arrapi=(
+    "network/any/" \
+    "network/host/" \
 		"network/dns_host/" \
 		"network/dns_group/" \
 		"network/group/" \
 		"network/range/" \
 		"network/network/" \
+		"network/interface_address/" \
+		"network/interface_broadcast/" \
 		"network/interface_network/" \
+		"network/multicast/" \
+		"network/aaa/" \
+		"service/ah/" \
+		"service/esp/" \
+		"service/any/" \
+		"service/icmp/" \
+		"service/icmpv6/" \
+		"service/ip/" \
 		"service/group/" \
 		"service/tcp/" \
 		"service/udp/" \
 		"service/tcpudp/" \
 		"packetfilter/packetfilter/" \
+		"packetfilter/nat/" \
+		"packetfilter/1to1nat/" \
+		"packetfilter/generic_proxy/" \
+		"packetfilter/group/" \
+		"packetfilter/loadbalance/" \
+		"packetfilter/mangle/" \
+		"packetfilter/masq/" \
+		"packetfilter/ruleset/" \
+
+		"geoip/geoipgroup/" \
+		"geoip/dstexception/" \
+		"geoip/srcexception/" \
+		"geoip/group/" \
+
+		"interface/bridge/" \
+		"interface/ethernet/" \
+		"interface/group/" \
+		"interface/tunnel/" \
+		"interface/vlan/" \
+
+		"ipsec/group/" \
+		"ipsec/policy/" \
+		"ipsec/remote_gateway/" \
+
+		"route/group/" \
+		"route/policy/" \
+		"route/static/" \
 )
 
 
 for i in "${arrapi[@]}"
 do
+	#curl -X GET -k -H "Accept: application/json" \
+	#      -H "Authorization: Basic $authkey" "$urlbase$i" -o "$TEMP_DIR/file$j.txt"
 	curl -X GET -k -H "Accept: application/json" \
-				-H "Authorization: Basic $apikey" "$urlbase$i" -o "$TEMP_DIR/file$j.txt"
+  				-H "Authorization: Basic $apikey" "$urlbase$i" -o "$TEMP_DIR/file$j.txt"
+
 	response_code=$?
 
 	if [ $response_code -eq 0 ]; then
