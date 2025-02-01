0804b15e38a7d04964b1055c40c76e349bdcb5ed
Sven Waschkut <swaschkut@users.noreply.github.com>
2025-02-01 01:45:28 +0100

    type=vendor-migration vendor=sophosxg | bring in export script

--- migration/parser/SOPHOSXG/SOPHOSXG.php
+++ migration/parser/SOPHOSXG/SOPHOSXG.php
@@ -40,36 +40,36 @@ class SOPHOSXG extends PARSER
 
             $XMLroot = $xml->documentElement;
 
-            if( strpos($filename, 'objects-ip.xml') !== false )
+            if( strpos($filename, 'IPHost') !== false )
             {
                 $this->sophos_xg_objectsIP($v, $XMLroot);
             }
-            if( strpos($filename, 'objects-ipgroup') !== false )
+            if( strpos($filename, 'IPHostGroup') !== false )
             {
                 $this->sophos_xg_objectsIPGROUP($v, $XMLroot);
             }
-            elseif( strpos($filename, 'objects-fqdn') !== false )
+            elseif( strpos($filename, 'FQDNHost') !== false )
             {
                 $this->sophos_xg_objectsFQDN($v, $XMLroot);
             }
-            elseif( strpos($filename, 'objects-service.xml') !== false )
+            elseif( strpos($filename, 'Services') !== false )
             {
                 $this->sophos_xg_objectsSERVICE($v, $XMLroot);
             }
-            elseif( strpos($filename, 'objects-servicegroup') !== false )
+            elseif( strpos($filename, 'ServiceGroup') !== false )
             {
                 $this->sophos_xg_objectsSERVICEGROUP($v, $XMLroot);
             }
-            elseif( strpos($filename, 'network-interfaces') !== false )
+            elseif( strpos($filename, 'Interface') !== false )
             {
                 $this->sophos_xg_networkINTERFACES($v, $XMLroot);
             }
-            elseif( strpos($filename, 'network-lags') !== false )
+            elseif( strpos($filename, 'LAG') !== false )
             {
                 //Todo: is this finalised???
                 $this->sophos_xg_networkLAGS($v, $XMLroot);
             }
-            elseif( strpos($filename, 'objects-mac') !== false )
+            elseif( strpos($filename, 'MACHost') !== false )
             {
                 //Todo: is this of interest?
                 /*
@@ -80,11 +80,11 @@ class SOPHOSXG extends PARSER
                       </MACHost>
                  */
             }
-            elseif( strpos($filename, 'network-vlans') !== false )
+            elseif( strpos($filename, 'VLAN') !== false )
             {
                 $this->sophos_xg_networkVLANS($v, $XMLroot);
             }
-            elseif( strpos($filename, 'network-zones') !== false )
+            elseif( strpos($filename, 'ZONE') !== false )
             {
                 //Todo: is this of interest?
                 //all information are used from Rules
@@ -121,15 +121,15 @@ class SOPHOSXG extends PARSER
                   </Zone>
                  */
             }
-            elseif( strpos($filename, 'routes-static') !== false )
+            elseif( strpos($filename, 'UnicastRoute') !== false )
             {
                 $this->sophos_xg_routeSTATIC($v, $XMLroot);
             }
-            elseif( strpos($filename, 'rules-firewall') !== false )
+            elseif( strpos($filename, 'FirewallRule') !== false )
             {
                 $this->sophos_xg_rulesFIREWALL($v, $XMLroot);
             }
-            elseif( strpos($filename, 'rules-nat') !== false )
+            elseif( strpos($filename, 'NATRule') !== false )
             {
                 #sophos_xg_rulesNAT($v, $XMLroot);
             }
@@ -169,53 +169,54 @@ class SOPHOSXG extends PARSER
         echo PH::boldText("\nVALIDATION - replace tmp services with APP-id if possible\n");
         CONVERTER::AppMigration( $v, $this->configType );
 
-//Todo:
 
 
-//Port 2 eth1/5 -> eth1/2
-//Port11 eth1/3-> eth1/8
-//Port12 eth1/4 -> ae1
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+/// CUSTOM
 
-//Port3 -> eth1/6
-//Port4 -> eth1/7
+        $custom = false;
 
-        $intDelete = array();
-        $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/1" );
-        $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/2" );
-        $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/8" );
-        $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/9" );
-        $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/10" );
-        $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/11" );
-        $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/12" );
-
-        foreach( $intDelete as $int )
+        if( $custom )
         {
-            /** @var EthernetInterface $int */
-            $int->owner->removeEthernetIf($int);
+            $intDelete = array();
+            $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/1" );
+            $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/2" );
+            $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/8" );
+            $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/9" );
+            $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/10" );
+            $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/11" );
+            $intDelete[] = $v->owner->network->ethernetIfStore->find( "ethernet1/12" );
+
+            foreach( $intDelete as $int )
+            {
+                $int->owner->removeEthernetIf($int);
+
+            }
+
+            $int = $v->owner->network->ethernetIfStore->find( "ethernet1/5" );
+            $int->setName("ethernet1/2");
+
+            $int = $v->owner->network->ethernetIfStore->find( "ethernet1/3" );
+            $int->setName("ethernet1/8");
+
+            $int = $v->owner->network->ethernetIfStore->find( "ethernet1/4" );
+            $int->setName("ae1");
+
+            if($v->owner->network->aggregateEthernetIfStore->xmlroot == null)
+                $v->owner->network->aggregateEthernetIfStore->createXmlRoot();
+
+            $v->owner->network->aggregateEthernetIfStore->xmlroot->appendChild($int->xmlroot->cloneNode(TRUE));
+            $int->xmlroot->parentNode->removeChild($int->xmlroot);
+
+
+
+
+            $int = $v->owner->network->ethernetIfStore->newEthernetIf( "ethernet1/3", "aggregate-group", "ae1" );
+            $int = $v->owner->network->ethernetIfStore->newEthernetIf( "ethernet1/4", "aggregate-group", "ae1" );
 
         }
-
-        $int = $v->owner->network->ethernetIfStore->find( "ethernet1/5" );
-        $int->setName("ethernet1/2");
-
-        $int = $v->owner->network->ethernetIfStore->find( "ethernet1/3" );
-        $int->setName("ethernet1/8");
-
-        $int = $v->owner->network->ethernetIfStore->find( "ethernet1/4" );
-        $int->setName("ae1");
-
-        if($v->owner->network->aggregateEthernetIfStore->xmlroot == null)
-            $v->owner->network->aggregateEthernetIfStore->createXmlRoot();
-
-        $v->owner->network->aggregateEthernetIfStore->xmlroot->appendChild($int->xmlroot->cloneNode(TRUE));
-        $int->xmlroot->parentNode->removeChild($int->xmlroot);
-
-
-
-
-        $int = $v->owner->network->ethernetIfStore->newEthernetIf( "ethernet1/3", "aggregate-group", "ae1" );
-        $int = $v->owner->network->ethernetIfStore->newEthernetIf( "ethernet1/4", "aggregate-group", "ae1" );
-
 
 
     }
