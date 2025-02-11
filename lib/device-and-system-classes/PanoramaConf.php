2830a3296dfe02af9e7f5634899de8dd41135e3f
Sven Waschkut <swaschkut@users.noreply.github.com>
2025-02-11 20:25:17 +0100

    timezone related part if timezone is null | bugfix

--- lib/device-and-system-classes/PanoramaConf.php
+++ lib/device-and-system-classes/PanoramaConf.php
@@ -1382,18 +1382,24 @@ class PanoramaConf
                     catch(Exception $e)
                     {
                         $timezone_backward = PH::timezone_backward_migration($this->timezone);
-                        $this->timezone = $timezone_backward;
-                        date_default_timezone_set($timezone_backward);
+                        if( $timezone_backward !== null )
+                        {
+                            $this->timezone = $timezone_backward;
+                            date_default_timezone_set($timezone_backward);
 
-                        PH::print_stdout("   --------------");
-                        PH::print_stdout( " X Timezone: $timezone->textContent is not supported with this PHP version. ".$this->timezone." is used." );
-                        PH::print_stdout("   - the timezone is IANA deprecated. Please change to a supported one:");
+                            PH::print_stdout("   --------------");
+                            PH::print_stdout( " X Timezone: $timezone->textContent is not supported with this PHP version. ".$this->timezone." is used." );
+                            PH::print_stdout("   - the timezone is IANA deprecated. Please change to a supported one:");
 
-
-                        PH::print_stdout();
-                        PH::print_stdout("   -- '".$this->timezone."'");
-                        PH::print_stdout("   --------------");
-                        PH::print_stdout();
+                            PH::print_stdout();
+                            PH::print_stdout("   -- '".$this->timezone."'");
+                            PH::print_stdout("   --------------");
+                            PH::print_stdout();
+                        }
+                        else
+                        {
+                            mwarning("timezone: '".$this->timezone."' not supported by IANA", null, false);
+                        }
                     }
                     PH::disableExceptionSupport();
                 }
