7f25028d2d085ae2e940eb849b9231625c9542ad
Sven Waschkut <swaschkut@users.noreply.github.com>
2026-07-27 07:08:33 +0200

    beta start working on 2.1.59

--- lib/misc-classes/PH.php
+++ lib/misc-classes/PH.php
@@ -254,7 +254,7 @@ class PH
 
     private static $library_version_major = 2;
     private static $library_version_sub = 1;
-    private static $library_version_bugfix = 58;
+    private static $library_version_bugfix = 59;
     private static $library_version_release = "beta";
 
     //BASIC AUTH PAN-OS 7.1
