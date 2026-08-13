ee39c53d193d0c177961f629fbc94996b6b7fdb9
Sven Waschkut <swaschkut@users.noreply.github.com>
2026-08-13 16:03:22 +0200

    another escaping fix

--- appid-toolbox/lib/old/lib_1_rule-marker.php
+++ appid-toolbox/lib/old/lib_1_rule-marker.php
@@ -139,9 +139,9 @@ function ruleMarker_Phase1_main( $subSystem, $configInput, $pan, $inputConnector
         $rule->setDescription($newDescription);
 
         if( $rule->isPostRule() )
-            $xmlPostRules .= "<entry name=\"{$rule->name()}\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
+            $xmlPostRules .= "<entry name=\"".htmlspecialchars( $rule->name() )."\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
         else
-            $xmlPreRules .= "<entry name=\"{$rule->name()}\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
+            $xmlPreRules .= "<entry name=\"".htmlspecialchars( $rule->name() )."\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
     }
 
     PH::print_stdout("\n\nNumber of rules marked: {$markedRules}    (vs already marked: {$alreadyMarked}");
--- appid-toolbox/lib/trait/lib_1_rule_marker.php
+++ appid-toolbox/lib/trait/lib_1_rule_marker.php
@@ -154,9 +154,9 @@ trait lib_1_rule_marker
             $rule->setDescription($newDescription);
 
             if( $rule->isPostRule() )
-                $xmlPostRules .= "<entry name=\"{$rule->name()}\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
+                $xmlPostRules .= "<entry name=\"".htmlspecialchars( $rule->name() )."\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
             else
-                $xmlPreRules .= "<entry name=\"{$rule->name()}\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
+                $xmlPreRules .= "<entry name=\"".htmlspecialchars( $rule->name() )."\"><description>" . htmlspecialchars($rule->description()) . "</description></entry>";
         }
 
         PH::print_stdout("\n\nNumber of rules marked: '{$markedRules}'    (vs already marked: '{$alreadyMarked}')");
